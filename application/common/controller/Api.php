ee96110629b5e74218a83f38dda9ab280fa32534
nessgbu <nessgbu@163.com>
2021-09-12 21:59:47 +0800

    Update Api.php

--- application/common/controller/Api.php
+++ application/common/controller/Api.php
@@ -187,4 +187,15 @@ class Api extends Base
         }
     }
 
+    public function test()
+    {
+        $config = config('xcxconf');
+        $appid = $config['appid'];
+        $secret = $config['secret'];
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$secret";
+        dump($url);
+        $jsonRes = curlGet($url);
+        dump($jsonRes);
+    }
+
 }
