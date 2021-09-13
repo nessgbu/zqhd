e307eee21e03962635f77ca01c35242599265107
nessgbu <nessgbu@163.com>
2021-09-13 16:58:04 +0800

    up

--- application/common/config/xcxconf.php
+++ application/common/config/xcxconf.php
@@ -8,9 +8,9 @@ array (
   'template_id' => 'xBeGlb8eI7rR62f_TNPedyx1QahyY2-akyqkt-YHGZA',
   'miniprogram_state' => 'developer',
   // 分享活动开始时间
-  'creatime' => '2021-09-13 15:40:00',
+  'creatime' => '2021-09-13 21:09:00',
   // 三兑一活动开始时间
-  'updatime' => '2021-09-16 11:00:00',
+  'updatime' => '2021-09-16 00:00:00',
   // 结束时间
   'endtime' => '2021-09-22 00:00:00',
 );
